192bf11fb7e55823cb8daeb073ec42ef07ea5eb2
Josan Liviu <l.josan@unifun.com>
2022-01-14 01:47:45 +0200

    Added middelware to log responses

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 Practical Test for the PHP Laravel 
 
 Run php artisan migrate:fresh --seed to generate data
+
+
+Logs in ./storage/logs/laravel.log
--- routes/api.php
+++ routes/api.php
@@ -20,6 +20,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 
-Route::middleware('auth.basic.once')->group(function () {
+Route::middleware('logger.responce')->group(function () {
     Route::apiResource('/books', BooksController::class);
 });
